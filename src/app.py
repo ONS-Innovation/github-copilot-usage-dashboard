c131fd9cc5b82a90052b4e7a88a5d4c2ddfe84cd
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2024-05-21 13:02:15 +0100

    add sample data

--- src/app.py
+++ src/app.py
@@ -1,3 +1,10 @@
 import streamlit as st
+import json
+import pandas as pd
 
-st.write("Hello World")
+st.title("Github Copilot Dashboard")
+
+with open("example_data/copilot_usage_data.json") as f:
+    data = json.load(f)
+
+st.json(data)
