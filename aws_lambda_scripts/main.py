1fa923c1a9a563a0ede7297411e07eef3628b1bb
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2024-08-20 10:44:04 +0100

    Added logging from default library to replace print logging

--- aws_lambda_scripts/main.py
+++ aws_lambda_scripts/main.py
@@ -2,6 +2,7 @@ import boto3
 from botocore.exceptions import ClientError
 import json
 import os
+import logging
 
 import github_api_toolkit
 
@@ -22,63 +23,85 @@ bucket_name = f"{account}-copilot-usage-dashboard"
 object_name = "historic_usage_data.json"
 file_name = "/tmp/historic_usage_data.json"
 
+logger = logging.getLogger()
 
 def handler(event, context):
     file_exists = True
+
+    logger.info("Starting process")
+
+    logger.info("Creating S3 client")
 
     # Create an S3 client
     session = boto3.Session()
     s3 = session.client('s3')
 
-    print("Session created")
+    logger.info("S3 client created")
+
+    logger.info("Getting historic_usage_data.json from S3")
 
     # Get historic_usage_data.json from S3
     try:
         s3.download_file(bucket_name, object_name, file_name)
     except ClientError as e:
-        print(f"Error getting historic_usage_data.json from S3: {e}")
+        logger.exception("Error getting historic_usage_data.json from S3")
         file_exists = False
     else:
-        print("Downloaded historic_usage_data.json from S3")
+        logger.info("Downloaded historic_usage_data.json from S3")
+
+    logger.info("Creating Secret Manager client")
 
     # Get the .pem file from AWS Secrets Manager
     secret_manager = session.client("secretsmanager", region_name=secret_reigon)
 
-    print("Secret Manager client created")
+    logger.info("Secret Manager client created")
+
+    logger.info("Getting secret from Secret Manager")
 
     secret = secret_manager.get_secret_value(SecretId=secret_name)["SecretString"]
 
-    print("Secret retrieved")
+    logger.info("Secret retrieved")
+
+    logger.info("Getting access token")
 
     # Get updated copilot usage data from GitHub API
     access_token = github_api_toolkit.get_token_as_installation(org, secret, client_id)
 
     if type(access_token) == str:
+        logger.error(f"Error getting access token: {access_token}")
         return(f"Error getting access token: {access_token}")
     else:
-        print("Access token retrieved")
+        logger.info("Access token retrieved")
+
+    logger.info("Creating API Controller")
 
     # Create an instance of the api_controller class
     gh = github_api_toolkit.github_interface(access_token[0])
 
-    print("API Controller created")
+    logger.info("API Controller created")
+
+    logger.info("Getting usage data from GitHub")
 
     # Get the usage data
     usage_data = gh.get(f"/orgs/{org}/copilot/usage")
     usage_data = usage_data.json()
 
-    print("Usage data retrieved")
+    logger.info("Usage data retrieved")
+
+    logger.info("Processing usage data")
 
     # If historic_usage_data.json exists, load it, else create an empty list
     if file_exists:
         with open(file_name, "r") as f:
             historic_usage = json.load(f)
-            print("Loaded historic_usage_data.json")
+            logger.info("Loaded historic_usage_data.json")
     else:
-        print("No historic_usage_data.json found, creating empty list")
+        logger.info("No historic_usage_data.json found, creating empty list")
         historic_usage = []
 
     dates_added = []
+
+    logger.info("Adding new usage data to historic_usage_data.json")
 
     # Append the new usage data to the historic_usage_data.json
     for day in usage_data:
@@ -87,16 +110,23 @@ def handler(event, context):
 
             dates_added.append(day["day"])
     
-    print(f"Added {len(dates_added)} new days to historic_usage_data.json: {dates_added}")
+    logger.info(
+        "New usage data added to historic_usage_data.json",
+        extra={
+            "no_days_added": len(dates_added),
+            "dates_added": dates_added
+        }
+    )
 
     # Write the updated historic_usage to historic_usage_data.json
     with open(file_name, "w") as f:
         f.write(json.dumps(historic_usage, indent=4))
-        print("Updated historic_usage_data.json")
+        logger.info("Written changes to historic_usage_data.json")
 
     # Upload the updated historic_usage_data.json to S3
     s3.upload_file(file_name, bucket_name, object_name)
 
-    print("Uploaded updated historic_usage_data.json to S3")
+    logger.info("Uploaded updated historic_usage_data.json to S3")
 
+    logger.info("Process complete")
     return("Process complete")
