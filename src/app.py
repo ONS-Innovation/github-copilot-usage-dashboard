1e8ed3f6a4adf0b09f4d3600a2491a387de09726
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2025-03-10 13:43:04 +0000

    Setup interim page for soph

--- src/app.py
+++ src/app.py
@@ -5,8 +5,11 @@ st.set_page_config(
     page_icon="./src/branding/ONS-symbol_digital.svg",
     layout="wide",
 )
-org_usage = st.Page("./pages/org_usage.py", title="Organization Usage", icon=":material/groups:")
-team_usage = st.Page("./pages/team_usage.py", title="Team Usage", icon=":material/group:")
+# org_usage = st.Page("./pages/org_usage.py", title="Organization Usage", icon=":material/groups:")
+# team_usage = st.Page("./pages/team_usage.py", title="Team Usage", icon=":material/group:")
 
-pg = st.navigation([org_usage, team_usage])
+interim_page = st.Page("./pages/interim_page.py", title="Interim Page", icon=":material/group:")
+
+# pg = st.navigation([org_usage, team_usage])
+pg = st.navigation([interim_page])
 pg.run()
---
+++ src/pages/interim_page.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.write("This is an interim page.")
+st.write("This page will be removed in the future.")
