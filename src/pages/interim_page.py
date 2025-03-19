d6f5bf1c8b1605fabe553645efd592abc984a56d
Sophie Stone <smstone04@gmail.com>
2025-03-19 13:42:01 +0000

    Add seat information

--- src/pages/interim_page.py
+++ src/pages/interim_page.py
@@ -40,8 +40,36 @@ secret_manager = session.client(service_name="secretsmanager", region_name=aws_d
 token = get_access_token(secret_manager, aws_secret_name, org, app_client_id) # Get Auth Token
 rest = github_api_toolkit.github_interface(token[0]) # Setup github_interface()
 
+# Get usage data
 response = rest.get('/orgs/ONSDigital/copilot/metrics')
 usage_data = response.json()
+
+# Get seat data
+response = rest.get('/orgs/ONSDigital/copilot/billing/seats', params={"per_page": 100})
+seat_data = response.json()
+
+try:
+    last_page = int(response.links["last"]["url"].split("=")[-1])
+except KeyError:
+    # If Key Error, Last doesn't exist therefore 1 page
+    last_page = 1
+    
+# Skip first page as we already have it
+for i in range(1, last_page):
+    response = rest.get("/orgs/ONSDigital/copilot/billing/seats", params={"per_page": 100, "page": i + 1})
+
+    seat_data["seats"].append(response.json()["seats"])
+
+df_seat_data = pd.DataFrame()
+
+# Puts the seat information from copilot_seats_data.json into a dataframe
+for row in seat_data["seats"]:
+    df_seat_data = pd.concat([df_seat_data, pd.json_normalize(row)], ignore_index=True)
+
+# Converts last_activity_at to a formatted string
+df_seat_data["last_activity_at"] = df_seat_data["last_activity_at"].apply(
+    lambda x: datetime.strptime(str(x), "%Y-%m-%dT%H:%M:%SZ") if pd.notna(x) and x not in (None, "") else None
+)
 
 min_date = datetime.strptime(usage_data[0]["date"], "%Y-%m-%d")
 max_date = datetime.strptime(usage_data[-1]["date"], "%Y-%m-%d")
@@ -145,7 +173,7 @@ st.write("This page serves as a fix for an update in GitHub's APIs by using the 
 st.header(":blue-background[IDE Code Completions]")
 
 # Display Metrics
-col1, col2, col3, col4 = st.columns(4)
+col1, col2, col3, col4, col5 = st.columns(5)
 col1.metric(
     "Total Acceptances",
     ide_completions_totals['code_acceptances'],
@@ -166,8 +194,7 @@ col4.metric(
     ide_completions_totals['lines_of_code_suggested'],
     border=True
     )
-
-st.metric(
+col5.metric(
     "Acceptance Rate",
     f"{round(ide_completions_totals['code_acceptances'] / ide_completions_totals['code_suggestions'] * 100, 2)}%",
     border=True
@@ -177,7 +204,7 @@ st.metric(
 st.header(":blue-background[CoPilot Chat]")
 
 # Display Metrics
-col1, col2, col3 = st.columns(3)
+col1, col2, col3, col4, col5 = st.columns(5)
 col1.metric(
     "Total Chats",
     copilot_chat_totals['total_chats'],
@@ -193,18 +220,88 @@ col3.metric(
     copilot_chat_totals['total_copies'],
     border=True
     )
-
-col1, col2 = st.columns(2)
-col1.metric(
+col4.metric(
     "Insert Rate",
     f"{round(copilot_chat_totals['total_insertions'] / copilot_chat_totals['total_chats'] * 100, 2)}%",
     border=True
     )
-col2.metric(
+col5.metric(
     "Copy Rate",
     f"{round(copilot_chat_totals['total_copies'] / copilot_chat_totals['total_chats'] * 100, 2)}%",
     border=True
     )
 
+# Seat information
 st.header(":blue-background[Seat Information]")
-# TODO: Add seat information here
+
+st.subheader("Inactivity Threshold")
+
+inactivity_threshold = st.number_input("Inactive after x days:", value=28, step=1)
+
+inactivity_date = datetime.now() - pd.Timedelta(days=inactivity_threshold)
+
+st.write("Users are considered inactive after:", inactivity_date.strftime("%-d %B %Y"))
+
+st.divider()
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.metric("Number of Seats", seat_data["total_seats"], border=True)
+
+with col2:
+    number_of_engaged_users = 0
+
+    for index, row in df_seat_data.iterrows():
+        if row.last_activity_at >= inactivity_date:
+            number_of_engaged_users += 1
+
+    st.metric("Number of Engaged Users", number_of_engaged_users, border=True)
+
+with col3:
+    number_of_inactive_users = seat_data["total_seats"] - number_of_engaged_users
+
+    st.metric("Number of Inactive Users", number_of_inactive_users, border=True)
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader("Active Users")
+
+    # Dataframe showing only active users (this with a latest activity)
+    st.dataframe(
+        df_seat_data.loc[df_seat_data["last_activity_at"] >= inactivity_date][
+            ["assignee.login", "last_activity_at", "assignee.html_url"]
+        ],
+        hide_index=True,
+        use_container_width=True,
+        column_config={
+            "assignee.login": st.column_config.Column("User"),
+            "last_activity_at": st.column_config.DatetimeColumn("Last Activity At", format="YYYY-MM-DD HH:mm"),
+            "assignee.html_url": st.column_config.LinkColumn(
+                "Github Profile",
+                help="A link to this user's profile",
+                display_text="Go to Profile",
+            ),
+        },
+    )
+
+with col2:
+    st.subheader("Inactive Users")
+
+    # Dataframe showing only inactive users (those where last_activity_at is None)
+    st.dataframe(
+        df_seat_data.loc[(df_seat_data["last_activity_at"].isnull()) | (df_seat_data["last_activity_at"] < inactivity_date)][
+            ["assignee.login", "last_activity_at", "assignee.html_url"]
+        ],
+        hide_index=True,
+        use_container_width=True,
+        column_config={
+            "assignee.login": st.column_config.Column("User"),
+            "last_activity_at": st.column_config.DatetimeColumn("Last Activity At", format="YYYY-MM-DD HH:mm"),
+            "assignee.html_url": st.column_config.LinkColumn(
+                "Github Profile",
+                help="A link to this user's profile",
+                display_text="Go to Profile",
+            ),
+        },
+    )
