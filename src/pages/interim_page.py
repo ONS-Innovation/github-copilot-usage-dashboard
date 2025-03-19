437104038d2d6109f33b2d92116c60a0a92cfd4c
Sophie Stone <smstone04@gmail.com>
2025-03-19 12:05:34 +0000

    Group datasets and display metrics on dashboard

--- src/pages/interim_page.py
+++ src/pages/interim_page.py
@@ -36,15 +36,11 @@ def get_access_token(secret_manager: Any, secret_name: str, org: str, app_client
     return token
 
 session = boto3.session.Session() # Create a Boto3 session
- 
 secret_manager = session.client(service_name="secretsmanager", region_name=aws_default_region) # Create a Secret Manager Client
-
 token = get_access_token(secret_manager, aws_secret_name, org, app_client_id) # Get Auth Token
- 
 rest = github_api_toolkit.github_interface(token[0]) # Setup github_interface()
 
 response = rest.get('/orgs/ONSDigital/copilot/metrics')
-
 usage_data = response.json()
 
 min_date = datetime.strptime(usage_data[0]["date"], "%Y-%m-%d")
@@ -62,7 +58,7 @@ date_range = st.slider(
     format="YYYY-MM-DD",
 )
 
-st.json(usage_data)
+# st.json(usage_data)
 
 @st.cache_data
 def generate_datasets(date_range: tuple):
@@ -80,20 +76,47 @@ def generate_datasets(date_range: tuple):
     ide_completions = pd.DataFrame()
 
     for i in range (len(df_usage_data_subset.date)):
-        print(df_usage_data_subset.date[i])
-        
+
         # Get copilot chat data
+        editors_data = df_usage_data_subset.iloc[i]["copilot_ide_chat.editors"]
 
-        # for model in df_usage_data_subset[i]["copilot_ide_chat_editors_models"]:
-        #     chat_data = pd.DataFrame([{
-        #         "total_chats": model.get("total_chats", 0),
-        #         "total_engaged_users": model.get("total_engaged_users", 0),
-        #         "total_copies": model.get("total_chat_copy_events", 0),
-        #         "total_insertions": model.get("total_chat_insertion_events", 0),
-        #     }])
-        #     copilot_chat = pd.concat([copilot_chat, chat_data], ignore_index=True)
+        for editor in editors_data:
+            editor_name = editor.get("name", "")
+            models_data = editor.get("models", [])
 
-        #TODO: Get IDE completions data
+            for model in models_data:
+                data = pd.DataFrame([{
+                    "editor_name": editor_name,
+                    "model_name": model.get("name", ""),
+                    "total_chats": model.get("total_chats", 0),
+                    "total_engaged_users": model.get("total_engaged_users", 0),
+                    "total_copies": model.get("total_chat_copy_events", 0),
+                    "total_insertions": model.get("total_chat_insertion_events", 0),
+                }])
+                copilot_chat = pd.concat([copilot_chat, data], ignore_index=True)
+
+
+        # Get IDE completions data
+        editors_data = df_usage_data_subset.iloc[i]["copilot_ide_code_completions.editors"]
+
+        for editor in editors_data:
+            editor_name = editor.get("name", "")
+            models_data = editor.get("models", [])
+
+            for model in models_data:
+                languages_data = model.get("languages", [])
+                for language in languages_data:
+                    data = pd.DataFrame([{
+                        "editor_name": editor_name,
+                        "model_name": model.get("name", ""),
+                        "language_name": language.get("name", ""),
+                        "engaged_users": language.get("total_engaged_users", 0),
+                        "code_acceptances": language.get("total_code_acceptances", 0),
+                        "code_suggestions": language.get("total_code_suggestions", 0),
+                        "lines_of_code_suggested": language.get("total_code_lines_suggested", 0),
+                        "lines_of_code_accepted": language.get("total_code_lines_accepted", 0),
+                    }])
+                    ide_completions = pd.concat([ide_completions, data], ignore_index=True)
 
     return (
         df_usage_data_subset,
@@ -107,40 +130,81 @@ def generate_datasets(date_range: tuple):
     ide_completions,
 ) = generate_datasets(date_range)
 
-#TODO: Calculate totals
-
+# Calculate totals
+copilot_chat_totals = copilot_chat[["total_chats", "total_engaged_users", "total_copies", "total_insertions"]].sum()
+ide_completions_totals = ide_completions[["engaged_users", "code_acceptances", "code_suggestions", "lines_of_code_suggested", "lines_of_code_accepted"]].sum()
 
 st.logo("./src/branding/ONS_Logo_Digital_Colour_Landscape_Bilingual_RGB.svg")
 
 col1, col2 = st.columns([0.8, 0.2])
 col1.title(":blue-background[Interim Page]")
 col2.image("./src/branding/ONS_Logo_Digital_Colour_Landscape_Bilingual_RGB.png")
-st.write("This page serves as a temporary fix for an update in GitHub's APIs by using the new API endpoints.")
+st.write("This page serves as a fix for an update in GitHub's APIs by using the new API endpoints.")
 
 # IDE Code Completions Metrics
 st.header(":blue-background[IDE Code Completions]")
 
 # Display Metrics
 col1, col2, col3, col4 = st.columns(4)
-col1.metric("Total Acceptances", 0, border=True)
-col2.metric("Total Suggestions", 0, border=True)
-col3.metric("Total Lines of Code Suggested", 0, border=True)
-col4.metric("Total Lines of Code Accepted", 0, border=True)
+col1.metric(
+    "Total Acceptances",
+    ide_completions_totals['code_acceptances'],
+    border=True
+    )
+col2.metric(
+    "Total Suggestions",
+    ide_completions_totals['code_suggestions'],
+    border=True
+    )
+col3.metric(
+    "Total Lines of Code Accepted",
+    ide_completions_totals['lines_of_code_accepted'],
+    border=True
+    )
+col4.metric(
+    "Total Lines of Code Suggested",
+    ide_completions_totals['lines_of_code_suggested'],
+    border=True
+    )
 
-st.metric("Acceptance Rate", 0, border=True)
+st.metric(
+    "Acceptance Rate",
+    f"{round(ide_completions_totals['code_acceptances'] / ide_completions_totals['code_suggestions'] * 100, 2)}%",
+    border=True
+    )
 
 # CoPilot Chat Metrics
 st.header(":blue-background[CoPilot Chat]")
 
 # Display Metrics
 col1, col2, col3 = st.columns(3)
-col1.metric("Total Sessions", 0, border=True)
-col2.metric("Total Insertions", 0, border=True)
-col3.metric("Total Copies", 0, border=True)
+col1.metric(
+    "Total Chats",
+    copilot_chat_totals['total_chats'],
+    border=True
+    )
+col2.metric(
+    "Total Insertions",
+    copilot_chat_totals['total_insertions'],
+    border=True
+    )
+col3.metric(
+    "Total Copies",
+    copilot_chat_totals['total_copies'],
+    border=True
+    )
 
 col1, col2 = st.columns(2)
-col1.metric("Insert Rate", 0, border=True)
-col2.metric("Copy Rate", 0, border=True)
+col1.metric(
+    "Insert Rate",
+    f"{round(copilot_chat_totals['total_insertions'] / copilot_chat_totals['total_chats'] * 100, 2)}%",
+    border=True
+    )
+col2.metric(
+    "Copy Rate",
+    f"{round(copilot_chat_totals['total_copies'] / copilot_chat_totals['total_chats'] * 100, 2)}%",
+    border=True
+    )
 
 st.header(":blue-background[Seat Information]")
 # TODO: Add seat information here
