13a23daac2b4653f65af4a0f6635572f76ab78f4
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-06-19 13:29:23 +0100

    reorder -f and -t

--- concourse/scripts/build_image.sh
+++ concourse/scripts/build_image.sh
@@ -7,7 +7,7 @@ container_image=$(echo "$github_copilot_secrets" | jq -r .container_image)
 
 aws ecr get-login-password --region eu-west-2 | podman --storage-driver=vfs login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com
 
-podman build -t -f=../../lambda_data_logger/Dockerfile ${container_image}:${tag} resource-repo
+podman build -t ${container_image}:${tag} resource-repo -f=../../lambda_data_logger/Dockerfile 
 
 podman tag ${container_image}:${tag} ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com/${container_image}:${tag}
 
