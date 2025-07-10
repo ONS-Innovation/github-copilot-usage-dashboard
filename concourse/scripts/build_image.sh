0a17c761d848e0c553fbbeba5b6315527c19f423
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-07-10 16:15:44 +0100

    refactor to use API for taggings

--- concourse/scripts/build_image.sh
+++ concourse/scripts/build_image.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 export STORAGE_DRIVER=vfs
 export PODMAN_SYSTEMD_UNIT=concourse-task
 
-container_image=$(echo "$github_copilot_secrets" | jq -r .container_image)
+container_image=$(echo "$secrets" | jq -r .container_image)
 
 aws ecr get-login-password --region eu-west-2 | podman --storage-driver=vfs login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com
 
