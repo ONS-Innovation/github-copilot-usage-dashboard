856d58964d1f1c1baf2a95ca731ea89300ea05f0
Sophie Stone <106412444+smstone0@users.noreply.github.com>
2025-08-08 09:51:40 +0100

    KEH-921-3 - Update branch and repo names (#60)
    * Replace all mentions of 'master' with 'main'
    
    * Replace mentions of 'dashboard' with 'lambda'
    - Remove deleted terraform from .gitignore

--- concourse/scripts/set_pipeline.sh
+++ concourse/scripts/set_pipeline.sh
@@ -11,7 +11,7 @@ else
     branch=$(git rev-parse --abbrev-ref HEAD)
 fi
 
-if [[ ${branch} == "main" || ${branch} == "master" ]]; then
+if [[ ${branch} == "main" ]]; then
     env="prod"
 else
     env="dev"
