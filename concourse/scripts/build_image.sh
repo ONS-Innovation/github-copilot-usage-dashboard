c5ab8892af4a9ef87b586d40e40d1e35529d8935
Sophie Stone <smstone04@gmail.com>
2025-08-21 13:21:39 +0100

    Handle bash linting

--- concourse/scripts/build_image.sh
+++ concourse/scripts/build_image.sh
@@ -1,14 +1,22 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC3040,SC2154,SC2148
 set -euo pipefail
 
 export STORAGE_DRIVER=vfs
 export PODMAN_SYSTEMD_UNIT=concourse-task
 
+# shellcheck disable=SC2154
 container_image=$(echo "$secrets" | jq -r .container_image)
 
+# shellcheck disable=SC2154
 aws ecr get-login-password --region eu-west-2 | podman --storage-driver=vfs login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com
 
+# shellcheck disable=SC2154
 podman build -t ${container_image}:${tag} resource-repo/src/
 
+# shellcheck disable=SC2154
 podman tag ${container_image}:${tag} ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com/${container_image}:${tag}
 
+# shellcheck disable=SC2154
 podman push ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com/${container_image}:${tag}
--- concourse/scripts/set_pipeline.sh
+++ concourse/scripts/set_pipeline.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
+# shellcheck disable=SC3040,SC2154,SC2148
 repo_name=${1}
 
 if [[ $# -gt 1 ]]; then
 	branch=${2}
 	git rev-parse --verify ${branch}
+	# shellcheck disable=SC2181
 	if [[ $? -ne 0 ]]; then
 		echo "Branch \"${branch}\" does not exist"
 		exit 1
