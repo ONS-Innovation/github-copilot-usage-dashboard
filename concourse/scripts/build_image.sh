aeabb1ea3c49dcef00fa88f4afd9bf8235e3e7ad
Sophie Stone <smstone04@gmail.com>
2025-08-21 13:31:13 +0100

    Add quotes around variables

--- concourse/scripts/build_image.sh
+++ concourse/scripts/build_image.sh
@@ -10,13 +10,13 @@ export PODMAN_SYSTEMD_UNIT=concourse-task
 container_image=$(echo "$secrets" | jq -r .container_image)
 
 # shellcheck disable=SC2154
-aws ecr get-login-password --region eu-west-2 | podman --storage-driver=vfs login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com
+aws ecr get-login-password --region eu-west-2 | podman --storage-driver=vfs login --username AWS --password-stdin "${aws_account_id}".dkr.ecr.eu-west-2.amazonaws.com
 
 # shellcheck disable=SC2154
-podman build -t ${container_image}:${tag} resource-repo/src/
+podman build -t "${container_image}":"${tag}" resource-repo/src/
 
 # shellcheck disable=SC2154
-podman tag ${container_image}:${tag} ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com/${container_image}:${tag}
+podman tag "${container_image}":"${tag}" "${aws_account_id}".dkr.ecr.eu-west-2.amazonaws.com/"${container_image}":"${tag}"
 
 # shellcheck disable=SC2154
-podman push ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com/${container_image}:${tag}
+podman push "${aws_account_id}".dkr.ecr.eu-west-2.amazonaws.com/"${container_image}":"${tag}"
