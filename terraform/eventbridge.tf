2ae2841a2ef9e99c83ee4e249e4ac09c7723796b
Sophie Stone <smstone04@gmail.com>
2025-08-21 16:00:22 +0100

    Format Terraform

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -1,5 +1,5 @@
 module "eventbridge" {
-  source = "terraform-aws-modules/eventbridge/aws"
+  source  = "terraform-aws-modules/eventbridge/aws"
   version = "4.1.0"
 
   role_name = "${var.lambda_name}-eventbridge-role"
